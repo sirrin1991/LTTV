58e8144df49f27c845c21b8d76e6529266e8f11b
sirrin1991 <sirrin1991@gmail.com>
2023-11-02 15:56:26 +0700

    update

---
+++ src/main/java/com/example/lttv/LttvApplication.java
@@ -0,0 +1,13 @@
+package com.example.lttv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LttvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LttvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lttv/LttvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lttv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LttvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
